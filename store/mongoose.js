2b84379579c7294c3bfd30483bb98f05742d1cef
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-13 17:06:57 +0430

    add useCreateIndex: true,

--- store/mongoose.js
+++ store/mongoose.js
@@ -4,6 +4,7 @@ module.exports.connect = async () => {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     user: process.env.MONGO_USER || "XXX",
     pass: process.env.MONGO_PASS || "XXX",
   };
