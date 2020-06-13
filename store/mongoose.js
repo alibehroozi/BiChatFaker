ecdd291e2c205e686081072abae81cb4024fc66e
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-13 04:58:33 +0430

    add user pass db

--- store/mongoose.js
+++ store/mongoose.js
@@ -5,9 +5,11 @@ module.exports.connect = async () => {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    user: "bichat",
+    pass: "4Kziq9UHiv3RPgDm",
   };
   const conn = mongoose.createConnection(
-    "mongodb+srv://bichat:4Kziq9UHiv3RPgDm@cluster0-wrr6v.mongodb.net/bichat",
+    "mongodb+srv://cluster0-wrr6v.mongodb.net/bichat",
     options
   );
   console.log("connected");
